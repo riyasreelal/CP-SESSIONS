8770e292a41d76637e61213591ba808ae30dde65
RIYA SREELAL <47321198+riyasreelal@users.noreply.github.com>
2020-04-16 21:03:30 +0530

    Update long_ATM_queue.cpp

--- session 1/long_ATM_queue.cpp
+++ session 1/long_ATM_queue.cpp
@@ -8,12 +8,12 @@ int main()
     int a[n];
     for(int i = 0; i < n; i++)
         std::cin>>a[i];
-    int cnt = 1;
-    for(int i = 1; i < n; i++)
+    int cnt = 1;   //number of groups
+    for(int i = 1; i < n; i++)    
     {
-        if(a[i]>=a[i-1])
+        if(a[i]>=a[i-1])   //checks groups based on increasing order of height
             continue;
-        else
+        else        //if a person is shorter than the person in front of him, a new group starts
             cnt++;
     }
     std::cout<<cnt;
