9692affdc191529646b7bc9d1c4aa595bbc887b5
RIYA SREELAL <47321198+riyasreelal@users.noreply.github.com>
2020-04-16 19:21:00 +0530

    Update Monk_and_Welcome.cpp

--- session 1/Monk_and_Welcome.cpp
+++ session 1/Monk_and_Welcome.cpp
@@ -6,20 +6,20 @@ int main()
 {
 	int n;
 	std::cin >> n;	
-	int A[n], B[n],C[n];
+	int A[n], B[n],C[n];		//input array 1
 	for(int i=0;i<n;i++)
 	{
 		std::cin>>A[i];
 	}
-	for(int i=0;i<n;i++)
+	for(int i=0;i<n;i++)		//input array 2
 	{
 		std::cin>>B[i];
 	}
-	for(int i=0;i<n;i++)
+	for(int i=0;i<n;i++)		//sum of both arraya
 	{
 		C[i]=A[i]+B[i];
 	}
-	for(int i=0;i<n;i++)
+	for(int i=0;i<n;i++)		//display sum
 	{
 		std::cout<<C[i]<<" ";
 	}			
