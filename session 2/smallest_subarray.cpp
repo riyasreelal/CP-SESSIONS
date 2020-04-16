80723f5f9faf83b57392d22c0da401b1b76a844a
RIYA SREELAL <47321198+riyasreelal@users.noreply.github.com>
2020-04-16 21:42:07 +0530

    Update smallest_subarray.cpp

--- session 2/smallest_subarray.cpp
+++ session 2/smallest_subarray.cpp
@@ -3,35 +3,38 @@
 vector<int> smallestSubarray(int sum, vector<int> input) 
 {
 	long int n=input.size();
-    long int curr_sum=0,l=n+1;
+    long int curr_sum=0,l=n+1;  //l is initailised to the length of largest sub array of given array
     int i = 0, j = 0,start,end;
     vector<int> subarray;
-    while (j < n) 
+    while (j < n) 	  //j is the index of the element upto which sum from a[0] < given sum
     { 
-        while (curr_sum <= sum && j < n) 
+        while (curr_sum <= sum && j < n)  
         { 
             if (curr_sum <= 0 && sum > 0) 
             { 
-                i = j; 
+                i = j;   
                 curr_sum = 0; 
             } 
   
-            curr_sum += input[j++]; 
+            curr_sum += input[j++];   //get the sum of elements smaller than given sum from 1st element onwards
         } 
-        while (curr_sum > sum && i < n) 
+        while (curr_sum > sum && i < n)   
         { 
-            if (j - i < l) 
+            if (j - i < l)      //to find the length of the minimum sub array
             {
                 l = j - i;
-                start=i;
-                end=j;
+                start=i;      //starting position is saved
+                end=j;	      //ending position is saved
             } 
-            curr_sum -= input[i++]; 
+	    /*if current sum exceeds the given sum, the initial elements are subtracted from the current sum
+	 	in order to bring the sum to a lower value*/
+		
+            curr_sum -= input[i++];   
         } 
     } 
-    for(i=start;i<end;i++)
+    for(i=start;i<end;i++)   //loop runs from starting position of sub array till end position
     {
-        subarray.push_back(input[i]); 
+        subarray.push_back(input[i]); //the values are pushed from the end into a new vector array
     }
-    return subarray;
+    return subarray; //the sub array is returned
 }
