545ea7e017dc9f0e22383752d9a3a2885a665102
RIYA SREELAL <47321198+riyasreelal@users.noreply.github.com>
2020-04-16 21:45:34 +0530

    Update stocks_are_profitable.java

--- session 2/stocks_are_profitable.java
+++ session 2/stocks_are_profitable.java
@@ -2,15 +2,15 @@ public class solution {
 
 	public static int maxProfit(final int stockprices[])
 	{
-		int minp = Integer.MAX_VALUE;
-        int maxp = 0;
+		int minp = Integer.MAX_VALUE; //the maximum value of stock prices is assigned to minmium price variable
+        int maxp = 0;  //maximum price variable is set to the lowest amount
         for (int i = 0; i < stockprices.length; i++) {
-            if (stockprices[i] < minp)
-                minp = stockprices[i];
-            else if (stockprices[i] - minp > maxp)
+            if (stockprices[i] < minp)     //finds the lowest stock price
+                minp = stockprices[i];    
+            else if (stockprices[i] - minp > maxp)  //finds the highest stock price which apears after the lowest price
                 maxp = stockprices[i] - minp;
         }
-        return maxp;
+        return maxp; //return the maximum stock value at which it can be sold
 	}
 }
 
